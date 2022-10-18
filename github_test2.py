68dc438eef371358b158359a4b11a41368e762aa
Thomas Treacy <treacyth@tcd.ie>
2022-10-18 13:08:41 +0100

    second test of adding code to first code branch

---
+++ github_test2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 18 13:02:06 2022
+
+@author: treacyth
+
+This is a second test of adding a file to github from folder 
+on laptop
+"""
+
